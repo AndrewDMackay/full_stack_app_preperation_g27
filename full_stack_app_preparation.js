15c8318fed8f965cd81013ff683932dcfccf5e24
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-19 10:18:40 +0100

    6th commit main

--- full_stack_app_preparation.js
+++ full_stack_app_preparation.js
@@ -1,51 +1,74 @@
 
 // Questions..
 
-// 1. What is responsible for defining the routes of the games resource?
+// What is responsible for defining the routes of the `games` resource?
 
-// server/create_router.js..
-
-
-// 2. What do you notice about the folder structure? 
-
-// At the top level the folder structure is divide into server, and client-side, which are then broken into sub-folders..
-
-// What's the client responsible for? 
+// `gamesRouter`
 
 
+// What do you notice about the folder structure? Whats the client responsible for? Whats the server responsible for?
 
-// What's the server responsible for?
-
-
-
-// 3. What are the the responsibilities of server.js?
+// The client is the code for the front-end, the UI.  The server is the back-end code
 
 
-// 4. What are the responsibilities of the gamesRouter?
+// What are the the responsibilities of server.js?
+
+// server.js is responsible for:
+
+// - using `express.json()`, which enables the server to access the body from requests
+// - serving static files (the client directory) to the browser
+// - enables cors (we'll discuss this in more detail later)
+// - connecting to the `games_hub` database using the MongoDB driver
+// - accessing the `games` collection from the database and pass it to `createRouter`
+// - delegating the routing for the `games` resource to `gamesRouter` on the path `/api/games`
+// - listening for requests being made on on a specific port
 
 
-// 5. What process does the the client (front-end) use to communicate with the server?
+// What are the responsibilities of the `gamesRouter`?
+
+// `gamesRouter` is responsible for defining the routes for the `games` resource and interacting with the database for each route as appropriate.
 
 
-// 6. What optional second argument does the fetch method take? And what is it used for in this application? 
-// Hint: See Using Fetch on the MDN docs..
+// What process does the the client (front-end) use to communicate with the server?
+
+// The client sends `fetch` requests via the `GamesService` to the server via the set of routes defined in the router..
+// [ e.g. 'localhost:3000/api/games' ]
+// The file is called `GamesService.js` as its responsible for making the connection with our games api.  You may also see this named `service.js` or `api` however if you're making calls to various end points its a good idea to name the file relating to the service its consuming.
 
 
+// What optional second argument does the `fetch` method take? And what is it used for in this application?
+
+// When `fetch` is passed only a URL as an argument, it makes a GET request by default..
+// However `fetch` takes an optional second argument of an `init` object that can be used to specify the request settings.
+
+// In this application the `request` helper's methods `post` and `delete` both pass `fetch` an object specifying the type of request. 
+// Additionally, in the case of the `post` method, it is also used to send the body of the request.. 
+// And, specify the body type as JSON using a header.
+
+// ```js
+// fetch('http://localhost:3000/api/games', {
+//   method: 'POST',
+//   body: JSON.stringify(game),
+//   headers: { 'Content-Type': 'application/json'}
+// })
+// .then(res => res.json())
+// ```
+
+// Which of the games API routes does the front-end application consume (i.e make requests to)?
+
+// - index (GET)
+// - create (POST)
+// - destroy (DELETE)
 
 
-// 7. Which of the games API routes does the front-end application consume (i.e. make requests to)?
+// What are we using the [MongoDB Driver](http://mongodb.github.io/node-mongodb-native/) for?
 
-// '/api/games'..
-
-
-// 8. What are we using the MongoDB Driver for?
-
-// Storing data, in this case, various objects from the app itself..
+// The MongoDB Driver is a library that enables us interact with the MongoDB database from inside our JavaScript application.
 
 
-// Extension..
+// ## Extension
 
-// 9. Why do we need to use ObjectId from the MongoDB driver?
+// Why do we need to use [`ObjectId`](https://mongodb.github.io/node-mongodb-native/api-bson-generated/objectid.html) from the MongoDB driver?
 
-// As MongoDB does not work via tables, we need to use the ObjectId type, as that is the only unique constant marker..
+// When the front-end makes a request regarding a specific game (SHOW, UPDATE, DELETE), the server access the ID of the particular game from the params object. This is always a string. To query the database for an object of a particular ID, if we ask it for the object with the ID of string type, it will never find a match. It needs us to make the query with an instance of `ObjectId`. We create the instance of `ObjectId` by passing in the ID as a string, for example, `ObjectId("5af17fe430e043c3e62149b8")`.
 
