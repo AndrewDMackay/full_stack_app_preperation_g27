028487381139ba4033a5c12d168749f90000b091
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-19 09:53:27 +0100

    5th commit main

--- full_stack_app_preparation.js
+++ full_stack_app_preparation.js
@@ -3,14 +3,19 @@
 
 // 1. What is responsible for defining the routes of the games resource?
 
-// server/create_router.js
+// server/create_router.js..
 
 
 // 2. What do you notice about the folder structure? 
 
-// Whats the client responsible for? 
+// At the top level the folder structure is divide into server, and client-side, which are then broken into sub-folders..
 
-// Whats the server responsible for?
+// What's the client responsible for? 
+
+
+
+// What's the server responsible for?
+
 
 
 // 3. What are the the responsibilities of server.js?
@@ -26,14 +31,21 @@
 // Hint: See Using Fetch on the MDN docs..
 
 
+
+
 // 7. Which of the games API routes does the front-end application consume (i.e. make requests to)?
+
+// '/api/games'..
 
 
 // 8. What are we using the MongoDB Driver for?
 
+// Storing data, in this case, various objects from the app itself..
 
 
 // Extension..
 
 // 9. Why do we need to use ObjectId from the MongoDB driver?
+
+// As MongoDB does not work via tables, we need to use the ObjectId type, as that is the only unique constant marker..
 
